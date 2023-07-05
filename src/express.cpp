5d13f1b8ec3ba6bb7b4ce4f973698b1cb70296e2
Miguel Barro <miguelbarro@eprosima.com>
2023-07-05 08:51:29 +0200

    improve expression grammar testing
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- src/express.cpp
+++ src/express.cpp
@@ -1,6 +1,7 @@
 // vim: tags+=~/Documents/DHI/PEGTL/taopeg.tags
 
 #include <iostream>
+#include <cstdlib>
 
 #include <tao/pegtl.hpp>
 #include <tao/pegtl/contrib/analyze.hpp>
@@ -49,10 +50,23 @@ template<typename Rule>
 struct report_action
 {
     template<typename Input>
-    static void apply(const Input& in)
+    static void apply(const Input& in, int& /*id*/)
     {
             using namespace std;
             cout << "Rule: " << typeid(Rule).name() << " " << in.string() << endl;
+    }
+};
+
+template<>
+struct report_action<scoped_or_literal>
+{
+    template<typename Input>
+    static void apply(const Input& in, int& id)
+    {
+            using namespace std;
+            ++id;
+            cout << "Rule: " << typeid(scoped_or_literal).name()
+                 << " " << in.string() << endl;
     }
 };
 
@@ -63,21 +77,33 @@ int main (int argc, char *argv[])
     std::size_t issues = tao::pegtl::analyze< my_grammar >(-1);
     if (issues > 0)
     {
-        issues = tao::pegtl::analyze< my_grammar >(1);
+        return tao::pegtl::analyze< my_grammar >(1);
     }
 
-    // Let's try to build it
-    if ( argc <= 1 )
+    // expected inputs:
+    // • expression to parse
+    // • expected identifiers to parse
+    // test passes if the expression is parsed properly and the number of identifiers matches
+    if ( argc <= 2 || argc > 3 )
         return -1;
 
-    pegtl::argv_input in( argv, 1 );
-    if( pegtl::parse<my_grammar, report_action>(in))
+    int res = 0;
+    int identified = 0;
+    int expected = atoi(argv[2]);
+
+    pegtl::argv_input in( argv, 1);
+
+    if( pegtl::parse<my_grammar, report_action>(in, identified))
     {
-        cout << "success!" << endl;
+        cout << "parsing success!" << endl;
+
+        // Check that literals are only parsed once
+        return identified - expected;
     }
     else {
         cerr << "I don't understand." << endl;
+        res = -1;
     }
 
-    return 0;
+    return res;
 }
