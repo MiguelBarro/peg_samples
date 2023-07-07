549b517c567fda9fcb5c61972e572a5399731b6f
Miguel Barro <miguelbarro@eprosima.com>
2023-07-07 10:00:13 +0200

    calculator actions
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- src/calc.cpp
+++ src/calc.cpp
@@ -26,39 +26,79 @@ struct report_action
     }
 };
 
-#define report_specialization(Rule, id) \
+#define load_action(Rule, id, stack) \
 template<> \
 struct report_action<Rule> \
 { \
     template<typename Input> \
-    static void apply(const Input& in, expr_reg& m, calc_stack&) \
+    static void apply(const Input& in, expr_reg& m, calc_stack& s) \
     { \
         m += (m.empty() ? "" : ";") + std::string{#id}; \
         cout << "Rule: " << typeid(Rule).name() \
              << " " << in.string() << endl; \
+        istringstream ss(in.string()); \
+        stack ; \
     } \
 };
 
-report_specialization(boolean_literal, bool)
-report_specialization(dec_literal, decimal)
-report_specialization(oct_literal, octal)
-report_specialization(hex_literal, hexa)
-report_specialization(float_literal, float)
-report_specialization(fixed_pt_literal, fixed)
+#define op_action(Rule, id, operation) \
+template<> \
+struct report_action<Rule> \
+{ \
+    template<typename Input> \
+    static void apply(const Input& in, expr_reg& m, calc_stack& s) \
+    { \
+        m += (m.empty() ? "" : ";") + std::string{#id}; \
+        cout << "Rule: " << typeid(Rule).name() \
+             << " " << in.string() << endl; \
+        operation ; \
+    } \
+};
 
-report_specialization(or_exec, or)
-report_specialization(xor_exec, xor)
-report_specialization(and_exec, and)
-report_specialization(rshift_exec, >>)
-report_specialization(lshift_exec, <<)
-report_specialization(add_exec, add)
-report_specialization(sub_exec, sub)
-report_specialization(mult_exec, mult)
-report_specialization(div_exec, div)
-report_specialization(mod_exec, mod)
-report_specialization(minus_exec, minus)
-report_specialization(plus_exec, plus)
-report_specialization(inv_exec, inv)
+template<>
+struct report_action<boolean_literal>
+{
+    template<typename Input>
+    static void apply(const Input& in, expr_reg& m, calc_stack& s)
+    {
+        cout << "Rule: " << typeid(boolean_literal).name()
+             << " " << in.string() << endl;
+
+        m += (m.empty() ? "" : ";") + std::string{"bool"};
+
+        struct custom_tf : std::numpunct<char> {
+            std::string do_truename()  const { return "TRUE"; }
+            std::string do_falsename() const { return "FALSE"; }
+        };
+
+        istringstream ss(in.string());
+        ss.imbue(locale(ss.getloc(), new custom_tf));
+
+        bool res;
+        ss >> boolalpha >> res;
+        s.emplace_back(res);
+    }
+};
+
+load_action(dec_literal, decimal, long long res; ss >> res; s.emplace_back(res))
+load_action(oct_literal, octal, long long res; ss >> setbase(ios_base::oct) >> res; s.emplace_back(res))
+load_action(hex_literal, hexa, long long res; ss >> setbase(ios_base::hex) >> res; s.emplace_back(res))
+load_action(float_literal, float, long double res; ss >> res; s.emplace_back(res))
+load_action(fixed_pt_literal, fixed, long double res; ss >> res; s.emplace_back(res); cout << res << endl)
+
+op_action(or_exec, or,)
+op_action(xor_exec, xor,)
+op_action(and_exec, and,)
+op_action(rshift_exec, >>,)
+op_action(lshift_exec, <<,)
+op_action(add_exec, add,)
+op_action(sub_exec, sub,)
+op_action(mult_exec, mult,)
+op_action(div_exec, div,)
+op_action(mod_exec, mod,)
+op_action(minus_exec, minus,)
+op_action(plus_exec, plus,)
+op_action(inv_exec, inv,)
 
 int main (int argc, char *argv[])
 {
@@ -90,8 +130,24 @@ int main (int argc, char *argv[])
         cout << "evaluated expressions: " << m << endl;
         cout << "expected expressions: " << argv[2] << endl;
 
-        // compare with expected
-        ret = m == argv[2] ? 0 : -1;
+        // compare with expected evaluation order
+        bool res = m == argv[2];
+
+        // compare evaluation result
+        if (s.front().type() == typeid(bool))
+        {
+            res = any_cast<bool>(s.front()) == (atoi(argv[3]) != 0);
+        }
+        else if (s.front().type() == typeid(long long))
+        {
+            res = any_cast<long long>(s.front()) == atoll(argv[3]);
+        }
+        else if (s.front().type() == typeid(long double))
+        {
+            res = any_cast<long double>(s.front()) == atof(argv[3]);
+        }
+
+        ret = res ? 0 : -1;
     }
     else {
         cerr << "I don't understand." << endl;
